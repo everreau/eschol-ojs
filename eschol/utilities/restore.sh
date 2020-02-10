b7421c7387c1425e961c1b9e0a7d6a4eb1b2114d
Martin Haye <martin.haye@ucop.edu>
2020-02-10 11:16:08 -0800

    Updates for new mysql version

--- eschol/utilities/restore.sh
+++ eschol/utilities/restore.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Restoring database from backup file."
-./singleToMultiTrans.rb ~/ojs/db_backup/ojs_db_dump.sql | mysql --defaults-extra-file=~/.passwords/ojs_dba_pw.mysql 
+./singleToMultiTrans.rb ~/ojs/db_backup/ojs_db_dump.sql | egrep -v 'SESSION.SQL_LOG_BIN|GTID_PURGED' | mysql --defaults-extra-file=~/.passwords/ojs_dba_pw.mysql 
